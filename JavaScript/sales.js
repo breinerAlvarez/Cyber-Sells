f0ffc7d9aa0b8252aa78c86d0c9c78ca8a136c2c
breinerAlvarez <alvarezbreiner02@gmail.com>
2021-10-01 00:10:00 -0500

    Made modification to sales management interface

--- JavaScript/sales.js
+++ JavaScript/sales.js
@@ -1,26 +1,56 @@
 // Variables
-var label = document.getElementById("label-frame");
-var iframe = document.getElementById('frame');
-var link = document.getElementById("link_1");
-var registro = document.getElementById("registrar");
-var ruta_factura = "sales-info.html";
-var ruta_registro = "sales-registro.html";
+let iframe = document.getElementById('frame');
+let link = document.getElementById("link_1");
+let registro = document.getElementById("registrar");
+let buscador = document.getElementById("buscador");
+let entradas = document.getElementById("inputs");
+let tabla = document.getElementById("tabla-frame");
+let salida = document.getElementById("off");
+let menu = document.getElementById("menu");
+let ruta_factura = "sales-info.html";
+let ruta_registro = "sales-registro.html";
+
 
 // Event listeners
-link.addEventListener("click", linkear);
 registro.addEventListener("click", registrar);
+link.addEventListener("click", linkear);
+buscador.addEventListener("click", mantainSecond);
+salida.addEventListener("click", salir);
+menu.addEventListener("click", mantain);
 
 // Funciones
 function linkear(){
   iframe.setAttribute("src", ruta_factura);
-  label.innerHTML = "Información de venta";
-  label.style.display="inherit";
 }
 
 function registrar(){
   iframe.setAttribute("src", ruta_registro);
-  label.innerHTML = "Registrar/Editar factura";
-  label.style.display="inherit";
 }
 
+function moverDown(){
+    tabla.style.marginTop="45px";
+}
 
+function moverUp(){
+  tabla.style.marginTop="12px";
+}
+
+function salir(){
+  alert("saliendo :-)")
+  salida.style.display="none";
+}
+
+function mantain(){
+  salida.style.display="block";
+}
+
+function mantainSecond(){
+  if(entradas.style.display=="block"){
+    entradas.style.display="none"
+    moverUp();
+  }else{
+    entradas.style.display ="block";
+    moverDown();
+  }
+  
+}
