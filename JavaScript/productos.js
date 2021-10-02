ad53012f325aa311dbc3fe5482c2c1dacbbab68d
breinerAlvarez <alvarezbreiner02@gmail.com>
2021-10-02 12:38:12 -0500

    Amend sales interface and integrate with products interface based upon Cinthia's work

--- JavaScript/productos.js
+++ JavaScript/productos.js
@@ -5,25 +5,27 @@ let registro = document.getElementById("registrar");
 let buscador = document.getElementById("buscador");
 let entradas = document.getElementById("inputs");
 let tabla = document.getElementById("tabla-frame");
-let menu = document.getElementById("menu");
 let salida = document.getElementById("off");
-let productos = document.getElementById("productos");
+let menu = document.getElementById("menu");
+let ventas = document.getElementById("ventas");
 // let usuarios = document.getElementById("usuarios");
-let ruta_factura = "sales-info.html";
-let ruta_registro = "sales-registro.html";
+let ruta_producto = "productos-info.html";
+let ruta_registro = "productos-registro.html";
+
+
 
 // Event listeners
 registro.addEventListener("click", registrar);
 link.addEventListener("click", linkear);
 buscador.addEventListener("click", mantainSecond);
+salida.addEventListener("click", salir);
 menu.addEventListener("click", mantain);
+ventas.addEventListener("click", changePage);
 // usuarios.addEventListener("click", changePage);
-productos.addEventListener("click", changePage);
-
 
 // Funciones
 function linkear() {
-  iframe.setAttribute("src", ruta_factura);
+  iframe.setAttribute("src", ruta_producto);
 }
 
 function registrar() {
@@ -57,6 +59,5 @@ function mantainSecond() {
 }
 
 function changePage() {
-  productos.setAttribute('href', './productos.html');
+  ventas.setAttribute('href', './sales.html');
 }
-
